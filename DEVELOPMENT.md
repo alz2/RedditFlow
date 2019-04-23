dd2559f0360dfb5be075a6a124221e89d48f6c50
Andrew Zhang <alz2@illinois.edu>
2019-04-23 14:53:37 -0500

    server push and development notes

---
+++ DEVELOPMENT.md
@@ -0,0 +1,17 @@
+Current Development Notes
+=========================
+
+## Backend
+
+[x] Implement Server Push for comments with Flask (Andrew)
+    * Tested with `curl -X GET localhost:8080/commentStream --no-buffer`
+
+[ ] Grab Historical 1-Day Reddit Dataset for a couple of hand selected subreddits and provide endpoint for the data
+
+## Frontend
+
+[ ] Test client side reception of server push clients
+
+[ ] Provide static `.html` pages with `bootstrap` modeled after mock-up
+
+[ ] Figure out how to display `.html` with React, or convert `.html` into React code
--- backend/test.py
+++ backend/test.py
@@ -1,13 +1,30 @@
 import praw
+import flask
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
+app = Flask(__name__)
+CORS(app)
 load_dotenv()
 reddit = praw.Reddit(client_id=os.environ['client_id'], 
                      client_secret=os.environ['client_secret'],
                      user_agent=os.environ['user_agent'])
 
-subreddit = reddit.subreddit('iama')
-for comment in subreddit.stream.comments(skip_existing=True):
-    print(comment)
-    print(comment.body)
+def subreddit_comment_stream(name):
+    subreddit = reddit.subreddit('iama')
+    for comment in subreddit.stream.comments(skip_existing=True):
+        print(comment)
+        print(comment.body)
+        yield(comment.body)
+
+@app.route('/commentStream')
+def comment_stream():
+    return flask.Response(subreddit_comment_stream('iama'),
+                          mimetype="text/event-stream")
+
+if __name__ == "__main__":
+    app.run(port=8080,
+            threaded=True,
+            debug=True)
