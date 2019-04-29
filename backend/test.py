de81ac5497797cf47540559a2af25a6baeff8653
Andrew Zhang <alz2@illinois.edu>
2019-04-29 16:10:11 -0500

    Streaming analyzed comments.

--- backend/test.py
+++ backend/test.py
@@ -1,23 +1,39 @@
 import praw
+import json
 import flask
 from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 CORS(app)
 load_dotenv()
+sid = SentimentIntensityAnalyzer()
 reddit = praw.Reddit(client_id=os.environ['client_id'], 
                      client_secret=os.environ['client_secret'],
                      user_agent=os.environ['user_agent'])
 
 def subreddit_comment_stream(name):
-    subreddit = reddit.subreddit('iama')
+    # postId,commentId,author,score,postDate,text,URL,flair,neutralScore,posScore,negScore
+    subreddit = reddit.subreddit('all')
     for comment in subreddit.stream.comments(skip_existing=True):
-        print(comment)
+        ss = sid.polarity_scores(comment.body)
         print(comment.body)
-        yield("data: " + comment.body + "\n\n")
+        res = {
+                "postId": comment.submission.id,
+                "commentId": comment.id,
+                "author": comment.author.name,
+                "score": comment.score,
+                "postDate": comment.created_utc,
+                "text": comment.body,
+                "URL": "",
+                "neutralScore": ss['neu'],
+                "posScore": ss['pos'],
+                "negScore": ss['neg']
+                }
+        yield("data: " + json.dumps(res) + "\n\n")
 
 @app.route('/commentStream')
 def comment_stream():
